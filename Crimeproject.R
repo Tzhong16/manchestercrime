2b4d1a806c13fe388b1b579f076c1fc291d58e9a
T.zhong16 <t.zhong16@gmail.com>
2017-01-26 23:57:00 +1300

    updated file

--- Crimeproject.R
+++ Crimeproject.R
@@ -22,13 +22,12 @@ mancr1611 %>%
   select(Month, Longitude, Latitude, `Crime type`) %>%   # ` ` used to select the column name which has space
   rename( Crimetype =`Crime type`) %>%  
   group_by(Crimetype) %>%
-  summarise(nCrime = n()) %>%
-  arrange(desc(nCrime)) -> crime1611
+  summarise(nCrime = n())  -> crime1611
 
 # g <- ggplot(mancr1611, aes(`Crime type`)) # sums the crime type
 # g + geom_bar()
 
-
+#plot bar
 # hjust scale 0-1 to set the theme location
 ggplot(crime1611, aes(Crimetype, nCrime, fill = Crimetype)) + 
   geom_bar(stat = "identity") +
@@ -37,5 +36,17 @@ ggplot(crime1611, aes(Crimetype, nCrime, fill = Crimetype)) +
   theme(plot.title = element_text(hjust = 0.5)) + 
   xlab("Crime type") + 
   ylab("Number of Crimes") 
+
+# reorder bar in geom_bar
+ggplot(crime1611, aes(x = reorder(Crimetype, -nCrime), nCrime, fill = Crimetype)) + 
+  geom_bar(stat = "identity") +
+  scale_x_discrete(labels = function(Crimetype) str_wrap(Crimetype, width = 10)) +
+  ggtitle("Criminal Numbers in Manchester, 2016 Nov") +    
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  xlab("Crime type") + 
+  ylab("Number of Crimes") 
  
+
+
+
 
